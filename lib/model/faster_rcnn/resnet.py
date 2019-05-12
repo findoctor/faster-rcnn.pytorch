96e04786d52217fef1c56b9d1644e88b1dd736ea
findoctor <2023063688@qq.com>
2019-05-12 12:13:18 +0200

    Update resnet.py
    adding deform cnn

--- lib/model/faster_rcnn/resnet.py
+++ lib/model/faster_rcnn/resnet.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from torch_deform_conv.layers import ConvOffset2D as ConvOffset2d
 from model.utils.config import cfg
 from model.faster_rcnn.faster_rcnn import _fasterRCNN
 
@@ -13,22 +14,24 @@ import math
 import torch.utils.model_zoo as model_zoo
 import pdb
 
+
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-       'resnet152']
+           'resnet152']
 
 
 model_urls = {
-  'resnet18': 'https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth',
-  'resnet34': 'https://s3.amazonaws.com/pytorch/models/resnet34-333f7ec4.pth',
-  'resnet50': 'https://s3.amazonaws.com/pytorch/models/resnet50-19c8e357.pth',
-  'resnet101': 'https://s3.amazonaws.com/pytorch/models/resnet101-5d3b4d8f.pth',
-  'resnet152': 'https://s3.amazonaws.com/pytorch/models/resnet152-b121ed2d.pth',
+    'resnet18': 'https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://s3.amazonaws.com/pytorch/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://s3.amazonaws.com/pytorch/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://s3.amazonaws.com/pytorch/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://s3.amazonaws.com/pytorch/models/resnet152-b121ed2d.pth',
 }
+
 
 def conv3x3(in_planes, out_planes, stride=1):
   "3x3 convolution with padding"
   return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-           padding=1, bias=False)
+                   padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -68,10 +71,10 @@ class Bottleneck(nn.Module):
 
   def __init__(self, inplanes, planes, stride=1, downsample=None):
     super(Bottleneck, self).__init__()
-    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False) # change
+    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)  # change
     self.bn1 = nn.BatchNorm2d(planes)
-    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, # change
-                 padding=1, bias=False)
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,  # change
+                           padding=1, bias=False)
     self.bn2 = nn.BatchNorm2d(planes)
     self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
     self.bn3 = nn.BatchNorm2d(planes * 4)
@@ -102,23 +105,73 @@ class Bottleneck(nn.Module):
     return out
 
 
+class deformBottleneck(nn.Module):
+  expansion = 4
+
+  def __init__(self, inplanes, planes, stride=1, downsample=None):
+    super(Bottleneck, self).__init__()
+    self.deform1 = ConvOffset2d(inplanes)
+    self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False)  # change
+    self.bn1 = nn.BatchNorm2d(planes)
+
+    self.deform2 = ConvOffset2d(planes)
+    self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,  # change
+                           padding=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(planes)
+
+    self.deform3 = ConvOffset2d(planes)
+    self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+    self.bn3 = nn.BatchNorm2d(planes * 4)
+    self.relu = nn.ReLU(inplace=True)
+    self.downsample = downsample
+    self.stride = stride
+
+  def forward(self, x):
+    residual = x
+
+    out = self.deform1(x)
+    out = self.conv1(x)
+    out = self.bn1(out)
+    out = self.relu(out)
+
+    out = self.deform2(x)
+    out = self.conv2(out)
+    out = self.bn2(out)
+    out = self.relu(out)
+
+    out = self.deform3(x)
+    out = self.conv3(out)
+    out = self.bn3(out)
+
+    if self.downsample is not None:
+      residual = self.downsample(x)
+
+    out += residual
+    out = self.relu(out)
+
+    return out
+
+
 class ResNet(nn.Module):
   def __init__(self, block, layers, num_classes=1000):
     self.inplanes = 64
     super(ResNet, self).__init__()
     self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                 bias=False)
+                           bias=False)
     self.bn1 = nn.BatchNorm2d(64)
     self.relu = nn.ReLU(inplace=True)
-    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0, ceil_mode=True) # change
+    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0, ceil_mode=True)  # change
     self.layer1 = self._make_layer(block, 64, layers[0])
     self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
     self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-    self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+    # self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
     # it is slightly better whereas slower to set stride = 1
     # self.layer4 = self._make_layer(block, 512, layers[3], stride=1)
+    self.layer4 = self._make_layer(deformBottleneck, 512, layers[3], stride=1)
     self.avgpool = nn.AvgPool2d(7)
     self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    self.finalConv = nn.Conv2d(2048, 1024, 1)
 
     for m in self.modules():
       if isinstance(m, nn.Conv2d):
@@ -132,9 +185,9 @@ class ResNet(nn.Module):
     downsample = None
     if stride != 1 or self.inplanes != planes * block.expansion:
       downsample = nn.Sequential(
-        nn.Conv2d(self.inplanes, planes * block.expansion,
-              kernel_size=1, stride=stride, bias=False),
-        nn.BatchNorm2d(planes * block.expansion),
+          nn.Conv2d(self.inplanes, planes * block.expansion,
+                    kernel_size=1, stride=stride, bias=False),
+          nn.BatchNorm2d(planes * block.expansion),
       )
 
     layers = []
@@ -155,10 +208,17 @@ class ResNet(nn.Module):
     x = self.layer2(x)
     x = self.layer3(x)
     x = self.layer4(x)
+    # Use new layer 4 with deformable-conv
 
+    '''
+    # remove last two layers
     x = self.avgpool(x)
     x = x.view(x.size(0), -1)
     x = self.fc(x)
+    '''
+
+    # Add 1*1 conv at last
+    x = self.finalConv(x)
 
     return x
 
@@ -217,6 +277,7 @@ def resnet152(pretrained=False):
     model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
   return model
 
+
 class resnet(_fasterRCNN):
   def __init__(self, classes, num_layers=101, pretrained=False, class_agnostic=False):
     self.model_path = 'data/pretrained_model/resnet101_caffe.pth'
@@ -230,13 +291,13 @@ class resnet(_fasterRCNN):
     resnet = resnet101()
 
     if self.pretrained == True:
-      print("Loading pretrained weights from %s" %(self.model_path))
+      print("Loading pretrained weights from %s" % (self.model_path))
       state_dict = torch.load(self.model_path)
-      resnet.load_state_dict({k:v for k,v in state_dict.items() if k in resnet.state_dict()})
+      resnet.load_state_dict({k: v for k, v in state_dict.items() if k in resnet.state_dict()})
 
     # Build resnet.
-    self.RCNN_base = nn.Sequential(resnet.conv1, resnet.bn1,resnet.relu,
-      resnet.maxpool,resnet.layer1,resnet.layer2,resnet.layer3)
+    self.RCNN_base = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu,
+                                   resnet.maxpool, resnet.layer1, resnet.layer2, resnet.layer3)
 
     self.RCNN_top = nn.Sequential(resnet.layer4)
 
@@ -247,21 +308,27 @@ class resnet(_fasterRCNN):
       self.RCNN_bbox_pred = nn.Linear(2048, 4 * self.n_classes)
 
     # Fix blocks
-    for p in self.RCNN_base[0].parameters(): p.requires_grad=False
-    for p in self.RCNN_base[1].parameters(): p.requires_grad=False
+    for p in self.RCNN_base[0].parameters():
+      p.requires_grad = False
+    for p in self.RCNN_base[1].parameters():
+      p.requires_grad = False
 
     assert (0 <= cfg.RESNET.FIXED_BLOCKS < 4)
     if cfg.RESNET.FIXED_BLOCKS >= 3:
-      for p in self.RCNN_base[6].parameters(): p.requires_grad=False
+      for p in self.RCNN_base[6].parameters():
+        p.requires_grad = False
     if cfg.RESNET.FIXED_BLOCKS >= 2:
-      for p in self.RCNN_base[5].parameters(): p.requires_grad=False
+      for p in self.RCNN_base[5].parameters():
+        p.requires_grad = False
     if cfg.RESNET.FIXED_BLOCKS >= 1:
-      for p in self.RCNN_base[4].parameters(): p.requires_grad=False
+      for p in self.RCNN_base[4].parameters():
+        p.requires_grad = False
 
     def set_bn_fix(m):
       classname = m.__class__.__name__
       if classname.find('BatchNorm') != -1:
-        for p in m.parameters(): p.requires_grad=False
+        for p in m.parameters():
+          p.requires_grad = False
 
     self.RCNN_base.apply(set_bn_fix)
     self.RCNN_top.apply(set_bn_fix)
@@ -286,3 +353,13 @@ class resnet(_fasterRCNN):
   def _head_to_tail(self, pool5):
     fc7 = self.RCNN_top(pool5).mean(3).mean(2)
     return fc7
+
+
+'''
+resnet_final = resnet(20, num_layers=101, pretrained=False, class_agnostic=False)
+print(resnet_final)
+
+y = resnet_final(torch.randn(10, 3, 64, 64))
+print('final output dimension of deform_ResNet:')
+print(y.size())
+'''
